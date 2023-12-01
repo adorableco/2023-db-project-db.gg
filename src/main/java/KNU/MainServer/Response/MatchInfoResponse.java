9ee7e66adf878e1951cdb1002bb0e5edba1d495c
Sanghee0820 <lsh277604@gmail.com>
2023-12-02 08:06:49 +0900

    feat: add 매치 response

--- src/main/java/KNU/MainServer/Response/MatchInfoResponse.java
+++ src/main/java/KNU/MainServer/Response/MatchInfoResponse.java
@@ -1,4 +1,14 @@
 package KNU.MainServer.Response;
 
+import KNU.MainServer.dto.GameAccountDTO;
+import KNU.MainServer.dto.MatchInfoDTO;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class MatchInfoResponse {
+    private GameAccountDTO gameAccount;
+    private List<MatchInfoDTO> matchInfos;
 }
