0df81bf871cbccd044f42b13793e194c94d6d63d
adorableco <tpdusdl1001@knu.ac.kr>
2023-12-02 09:19:15 +0900

    Feat: "소환사 검색 시 소환사 리스트 페이지로"
    * 기존 : 소환사 검색 시 소환사의 매치 리스트로
    * 변경 : 소환사 이름 검색 시 일단 관련 있는 (이름이 비슷한) 소환사 리스트로
    
    resolves : #1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,17 @@ import Search from "./component/Search";
 import SearchResultPage from "./component/SearchResultPage";
 import MatchDetailPage from "./component/MatchDetailPage";
 import AdminPage from "./component/AdminPage";
+import SummonerListPage from "./component/SummonerListPage";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Search />} />
-        <Route path='/summoner/matches' element={<MatchDetailPage />} />
         <Route path='/admin' element={<AdminPage />} />
+        <Route path='/' element={<Search />} />
+        <Route path='/summoner-list' element={<SummonerListPage />} />
         <Route
-          path='/summoner/matches/match_id'
+          path='/search-result/:summonerId'
           element={<MatchDetailPage />}
         />
       </Routes>
--- frontend/src/component/Search.js
+++ frontend/src/component/Search.js
@@ -1,60 +1,40 @@
 /** @format */
 
-import React from "react";
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SearchResultPage from "./SearchResultPage";
-import MatchDetailPage from "./MatchDetailPage";
+// Search.js
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Search.css";
-import axios from "axios";
-import logo from "../img/logo.PNG";
 
 const Search = () => {
   const [summonerName, setSummonerName] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
+  const navigate = useNavigate();
 
   const handleInputChange = (e) => {
     setSummonerName(e.target.value);
   };
 
-  const handleSearch = async () => {
-    await axios.get(`/summoners/match?name=${summonerName}`);
-    console.log("Searching for summoner:", summonerName);
+  const handleSearch = () => {
+    // 실제로는 소환사 검색 로직이 들어갑니다.
+    // ...
 
-    // 가상의 매치 데이터 예시
-    const fakeMatchData = [
-      { matchId: "1", result: "Win" },
-      { matchId: "2", result: "Loss" },
-      // ... 더 많은 매치 데이터
-    ];
-
-    setSearchResult(fakeMatchData);
+    // 임시로 SummonerListPage로 이동
+    navigate("/summoner-list");
   };
 
   return (
     <div className='app'>
-      <img src={logo} style={{ width: "60px" }} />
       <h3 style={{ marginBottom: "0px" }}>롤 매치의 모든 정보</h3>
-      <h1 style={{ marginTop: "0px" }}> 롤 전적 검색 서비스</h1>
+      <h1 style={{ marginTop: "0px" }}>소환사 검색</h1>
       <div className='search-container'>
         <input
           type='text'
           placeholder='소환사명을 입력하세요'
           value={summonerName}
           onChange={handleInputChange}
-          required
-          style={{ margin: "0px", marginRight: "10px" }}
         />
         <button onClick={handleSearch}>검색</button>
       </div>
-
-      <Routes>
-        <Route
-          path='/search-result'
-          element={<SearchResultPage matches={searchResult} />}
-        ></Route>
-        <Route path='/match/:matchId' element={<MatchDetailPage />}></Route>
-      </Routes>
     </div>
   );
 };
