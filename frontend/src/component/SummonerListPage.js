f9eb9bf80cd4aed9dece81b94792420fce02591d
adorableco <tpdusdl1001@knu.ac.kr>
2023-12-05 01:28:11 +0900

    ✨ Feat: "이름이 비슷한 사용자 찾기"
     소환사 이름 검색 시 유사한 소환사 리스트 출력
    이 소환사 리스트에서 특정 소환사의 매치 목록을 볼 수 있도록 Link 예정
    
    resolves: #14

--- frontend/src/component/SummonerListPage.js
+++ frontend/src/component/SummonerListPage.js
@@ -3,32 +3,36 @@
 // SummonerListPage.js
 
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, useLocation, useParams } from "react-router-dom";
+import "./SummonerListPage.css";
 const SummonerListPage = () => {
-  const [summoners, setSummoners] = useState([
-    { id: 1, name: "Summoner1" },
-    { id: 2, name: "Summoner2" },
-    // ... 더 많은 소환사 데이터
-  ]);
-
+  const { state } = useLocation();
+  const { searchKeyword } = useParams();
+  const [summoners, setSummoners] = useState(state.gameAccounts);
   const navigate = useNavigate();
 
-  const handleSummonerClick = (summonerId) => {
+  console.log(state);
+  const handleSummonerClick = (summoner) => {
     // 클릭한 소환사에 해당하는 SearchResultPage 경로로 이동
-    navigate(`/search-result/${summonerId}`);
+    navigate(`/match_list/${summoner.gameName}`, { state: summoner });
   };
 
   return (
-    <div>
-      <h2>소환사 리스트</h2>
-      <ul>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h2>"{searchKeyword}" (으)로 시작하는 소환사 이름 검색 결과</h2>
+      <ul className='link-container'>
         {summoners.map((summoner) => (
-          <li
-            key={summoner.id}
-            onClick={() => handleSummonerClick(summoner.id)}
-          >
-            {summoner.name}
+          <li key={summoner.id} onClick={() => handleSummonerClick(summoner)}>
+            <p>
+              게임 이름 : {summoner.gameName} 게임 레벨 :{" "}
+              {summoner.accountLevel}
+            </p>
           </li>
         ))}
       </ul>
