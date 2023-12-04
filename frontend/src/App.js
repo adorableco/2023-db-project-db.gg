076c382ca9782eaaa839d7efaa26b2593462b99b
adorableco <tpdusdl1001@knu.ac.kr>
2023-12-05 01:40:33 +0900

    📤 Docs: "package.json 변경"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,10 @@ import SearchResultPage from "./component/SearchResultPage";
 import MatchDetailPage from "./component/MatchDetailPage";
 import AdminPage from "./component/AdminPage";
 import SummonerListPage from "./component/SummonerListPage";
+import EditUser from "./component/EditUser";
+import EditItem from "./component/EditItem";
+import ItemAverage from "./component/ItemAverage";
+import Champion from "./component/Champion";
 
 function App() {
   return (
@@ -15,11 +19,16 @@ function App() {
       <Routes>
         <Route path='/admin' element={<AdminPage />} />
         <Route path='/' element={<Search />} />
-        <Route path='/summoner-list' element={<SummonerListPage />} />
         <Route
-          path='/search-result/:summonerId'
-          element={<MatchDetailPage />}
+          path='/summoner_list/:searchKeyword'
+          element={<SummonerListPage />}
         />
+        <Route path='/match_list/:summonerId' element={<SearchResultPage />} />
+        <Route path='/match_detail/:matchId' element={<MatchDetailPage />} />
+        <Route path='/user_edit' element={<EditUser />} />
+        <Route path='/item_edit' element={<EditItem />} />
+        <Route path='/item_average/:kills' element={<ItemAverage />} />
+        <Route path='/champ/:champName' element={<Champion />} />
       </Routes>
     </div>
   );
