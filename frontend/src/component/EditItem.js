ab436d7d4a0397472cc5bf46afd9cc86ee4ed530
adorableco <tpdusdl1001@knu.ac.kr>
2023-12-05 21:42:57 +0900

    ✨ Feat: "아이템 추가/수정/삭제"
    resolves: #30

--- frontend/src/component/EditItem.js
+++ frontend/src/component/EditItem.js
@@ -4,33 +4,34 @@
 
 import React, { useState, useEffect } from "react";
 import "./EditUser.css";
+import axios from "axios";
 
 const mockData = {
-  gameAccounts: [
+  items: [
     {
-      gameName: "Noob piss off",
-      tier: "CHALLENGER",
-      accountLevel: 300,
+      name: "Noob piss off",
+      price: "CHALLENGER",
+      description: 300,
     },
     {
-      gameName: "NS Calix",
-      tier: "CHALLENGER",
-      accountLevel: 859,
+      name: "NS Calix",
+      price: "CHALLENGER",
+      description: 859,
     },
     {
-      gameName: "NEED HER",
-      tier: "MASTER",
-      accountLevel: 843,
+      name: "NEED HER",
+      price: "MASTER",
+      description: 843,
     },
     {
-      gameName: "No way out",
-      tier: "MASTER",
-      accountLevel: 118,
+      name: "No way out",
+      price: "MASTER",
+      description: 118,
     },
     {
-      gameName: "Naind",
-      tier: "MASTER",
-      accountLevel: 102,
+      name: "Naind",
+      price: "MASTER",
+      description: 102,
     },
   ],
 };
@@ -39,110 +40,138 @@ const EditItem = () => {
   const [isAddFormVisible, setIsAddFormVisible] = useState(false);
   const [isEditFormVisible, setIsEditFormVisible] = useState(false);
   const [isDeleteFormVisible, setIsDeleteFormVisible] = useState(false);
-  const [existingAccount, setExistingAccount] = useState(false);
-  const [gameName, setGameName] = useState("");
-  const [newGameName, setNewGameName] = useState("");
-  const [tier, setTier] = useState("");
-  const [accountLevel, setAccountLevel] = useState("");
+  const [existingitem, setExistingitem] = useState(false);
+  const [name, setName] = useState("");
+  const [newname, setNewName] = useState("");
+  const [newItemId, setNewItemId] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
   const [editing, setEditing] = useState(false);
   const [canTouch, setCanTouch] = useState(false);
-  const [gameAccounts, setGameAccounts] = useState([]);
+  const [items, setItems] = useState([]);
+  const [updateditems, setUpdateditems] = useState([]);
 
   const handleInputChange = (e) => {
-    setGameName(e.target.value);
+    setName(e.target.value);
   };
 
-  const handleNewGameNameChange = (e) => {
-    setNewGameName(e.target.value);
+  const handleNewnameChange = (e) => {
+    setNewName(e.target.value);
   };
 
-  const handleTierChange = (e) => {
-    setTier(e.target.value);
+  const handlepriceChange = (e) => {
+    setPrice(e.target.value);
   };
 
-  const handleAccountLevelChange = (e) => {
-    setAccountLevel(e.target.value);
+  const handledescriptionChange = (e) => {
+    setDescription(e.target.value);
   };
 
-  const handleAdd = () => {
-    const newGameAccount = {
-      gameName: newGameName,
-      tier,
-      accountLevel,
-    };
+  //api 연결해야 할 곳
+  const handleAdd = async () => {
+    try {
+      await axios
+        .post(`/api`, {
+          itemId: 3187,
+          name: "새로운아이템",
+          description: "설명",
+          price: 100,
+        })
+        .then((res) => {
+          // 중복 여부 체크
+          // if(res.data == "이미 존재하는 아이템입니다."){
+          //   alert("이미 존재하는아이템입니다.")
+          // }
 
-    setGameAccounts([...gameAccounts, newGameAccount]);
-    clearInputs();
+          console.log(res.data);
+        });
+      const newItem = {
+        itemId: newItemId,
+        name: newname,
+        price,
+        description,
+      };
+
+      setItems([...items, newItem]);
+      clearInputs();
+    } catch {
+      console.error("Adding Item Failed!");
+    }
   };
 
+  //api 연결해야 할 곳
   const handleEdit = () => {
     if (canTouch) {
-      const editedGameAccounts = gameAccounts.map((account) =>
-        account.gameName === gameName
-          ? { ...account, gameName: newGameName, tier, accountLevel }
-          : account,
+      const editeditems = items.map((item) =>
+        item.name === name
+          ? { ...item, name: newname, price, description }
+          : item,
       );
 
-      setGameAccounts(editedGameAccounts);
+      setItems(editeditems);
       setEditing(false);
       clearInputs();
     } else {
-      alert("'게임 이름 확인 버튼'을 눌러 존재 여부를 확인하세요.");
+      alert("'아이템 이름 확인 버튼'을 눌러 존재 여부를 확인하세요.");
       clearInputs();
     }
   };
 
-  const handleDelete = () => {
-    const updatedGameAccounts = gameAccounts.filter(
-      (account) => account.gameName !== gameName,
-    );
+  //api 연결해야 할 곳
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/?`).then(() => {
+        alert("삭제가 완료되었습니다.");
+        // const updateditems = items.filter(
+        //   (item) => item.name !== name,
+        // );
+      });
+    } catch {
+      console.error("Deleting Item Failed!");
+    }
 
-    setGameAccounts(updatedGameAccounts);
+    setItems(updateditems);
     clearInputs();
   };
 
-  const handleEditClick = (account) => {
-    setGameName(account.gameName);
-    setNewGameName(account.gameName);
-    setTier(account.tier);
-    setAccountLevel(account.accountLevel);
+  const handleEditClick = (item) => {
+    setName(item.name);
+    setNewName(item.name);
+    setPrice(item.price);
+    setDescription(item.description);
     setEditing(true);
   };
 
   // 수정/삭제 시 회원 존재 확인
   const checkExistedName = () => {
-    setExistingAccount(
-      mockData.gameAccounts.find((account) => account.gameName === gameName),
-    );
-    if (!existingAccount) {
-      alert("존재하지 않는 게임 이름입니다.");
-      setGameName("");
+    setExistingitem(mockData.items.find((item) => item.name === name));
+    if (!existingitem) {
+      alert("존재하지 않는 아이템 이름입니다.");
+      setName("");
     }
   };
 
   // 수정/등록 시 새 이름 중복 여부 확인
   const checkSameName = () => {
-    setExistingAccount(
-      mockData.gameAccounts.find((account) => account.gameName === gameName),
-    );
-    if (existingAccount) {
-      alert("이미 존재하는 게임 이름입니다.");
-      setNewGameName("");
+    setExistingitem(mockData.items.find((item) => item.name === name));
+    if (existingitem) {
+      alert("이미 존재하는 아이템 이름입니다.");
+      setNewName("");
     } else {
-      alert("사용 가능한 게임 이름입니다.");
+      alert("사용 가능한 아이템 이름입니다.");
     }
   };
 
   const clearInputs = () => {
-    setGameName("");
-    setNewGameName("");
-    setTier("");
-    setAccountLevel("");
+    setName("");
+    setNewName("");
+    setPrice("");
+    setDescription("");
     setCanTouch(false);
   };
 
   useEffect(() => {
-    setGameAccounts(mockData.gameAccounts);
+    setItems(mockData.items);
   }, []);
 
   const toggleForms = (formType) => {
@@ -156,30 +185,26 @@ const EditItem = () => {
 
   return (
     <div>
-      <h1>사용자 추가/수정/삭제</h1>
+      <h1>아이템 추가/수정/삭제</h1>
       <div>
-        <button onClick={() => toggleForms("add")}>Add Account</button>
+        <button onClick={() => toggleForms("add")}>Add item</button>
 
-        <button onClick={() => toggleForms("edit")}>Edit Account</button>
+        <button onClick={() => toggleForms("edit")}>Edit item</button>
 
-        <button onClick={() => toggleForms("delete")}>Delete Account</button>
+        <button onClick={() => toggleForms("delete")}>Delete item</button>
       </div>
       <div>
         {isAddFormVisible && (
           <>
-            <label>Game Name:</label>
-            <input
-              type='text'
-              value={newGameName}
-              onChange={handleNewGameNameChange}
-            />
-            <label>Tier:</label>
-            <input type='text' value={tier} onChange={handleTierChange} />
-            <label>Account Level:</label>
+            <label>Item Name:</label>
+            <input type='text' value={newname} onChange={handleNewnameChange} />
+            <label>price:</label>
+            <input type='text' value={price} onChange={handlepriceChange} />
+            <label>item Level:</label>
             <input
               type='number'
-              value={accountLevel}
-              onChange={handleAccountLevelChange}
+              value={description}
+              onChange={handledescriptionChange}
             />
             <button onClick={handleAdd}>Add</button>
           </>
@@ -187,25 +212,21 @@ const EditItem = () => {
 
         {isEditFormVisible && (
           <>
-            <label>Game Name:</label>
-            <input type='text' value={gameName} onChange={handleInputChange} />
-            <button onClick={checkExistedName}>수정할 게임 이름 확인</button>
+            <label>Item Name:</label>
+            <input type='text' value={name} onChange={handleInputChange} />
+            <button onClick={checkExistedName}>수정할 아이템 이름 확인</button>
 
-            <label>New Game Name:</label>
-            <input
-              type='text'
-              value={newGameName}
-              onChange={handleNewGameNameChange}
-            />
+            <label>New Item Name:</label>
+            <input type='text' value={newname} onChange={handleNewnameChange} />
             <button onClick={checkSameName}>중복 여부 확인</button>
 
-            <label>Tier:</label>
-            <input type='text' value={tier} onChange={handleTierChange} />
-            <label>Account Level:</label>
+            <label>price:</label>
+            <input type='text' value={price} onChange={handlepriceChange} />
+            <label>item Level:</label>
             <input
               type='number'
-              value={accountLevel}
-              onChange={handleAccountLevelChange}
+              value={description}
+              onChange={handledescriptionChange}
             />
             <button onClick={handleEdit}>Edit</button>
           </>
@@ -213,23 +234,23 @@ const EditItem = () => {
 
         {isDeleteFormVisible && (
           <>
-            <label>Game Name:</label>
-            <input type='text' value={gameName} onChange={handleInputChange} />
-            <button onClick={checkExistedName}>게임 이름 확인</button>
+            <label>Item Name:</label>
+            <input type='text' value={name} onChange={handleInputChange} />
+            <button onClick={checkExistedName}>아이템 이름 확인</button>
 
             <button onClick={handleDelete}>Delete</button>
           </>
         )}
       </div>
       <div>
-        <h2 style={{ marginTop: "40px" }}>게임 계정 정보</h2>
+        <h2 style={{ marginTop: "40px" }}>아이템 정보</h2>
         <ul>
-          {gameAccounts.map((account) => (
-            <li key={account.gameName}>
-              <span>{account.gameName}</span>
-              <span>{account.tier}</span>
-              <span>{account.accountLevel}</span>
-              <button onClick={() => handleEditClick(account)}>Edit</button>
+          {items.map((item) => (
+            <li key={item.name}>
+              <span>이름 : {item.name}</span>
+              <span>가격 : {item.price}</span>
+              <span>설명 : {item.description}</span>
+              <button onClick={() => handleEditClick(item)}>Edit</button>
             </li>
           ))}
         </ul>
